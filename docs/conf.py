5df9c87066972019524846a3a66f80f1ce6fa5ac
Vito Squicciarini <78115109+vsquicciarini@users.noreply.github.com>
2025-02-24 15:34:12 +0000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,28 @@
-html_theme = 'sphinx_rtd_theme'
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-# override the default css
-html_context = { 'css_files': ['_static/css/theme_overrides.css', ],}
+project = 'Test Project'
+copyright = '2000-2042, The Test Project Authors'
+author = 'The Authors'
+version = release = '4.16'
 
-# Add relative template path.
-templates_path = ['../source']
+# -- General configuration ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+exclude_patterns = [
+    '_build',
+    'Thumbs.db',
+    '.DS_Store',
+]
+extensions = []
+language = 'en'
+master_doc = 'index'
+pygments_style = 'sphinx'
+source_suffix = '.rst'
+templates_path = ['_templates']
+
+# -- Options for HTML output ----------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
